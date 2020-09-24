eb64b0cd0e15275cc19e1d79553e000256f249d5
Jack <git clone https://j4688821105@bitbucket.org/etcapp/jack.git>
2020-09-24 20:46:47 +0800

    debug v2

--- app/src/main/java/com/example/myapplication/ViewModel/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ViewModel/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
+import com.example.myapplication.cache.MemoryCache;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static MemoryCache memoryCache = new MemoryCache();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
